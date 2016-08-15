d8c60a032e123d74f566b86bca474dd2e5c56cda
Jiri <jiri.roznovjak@gmail.com>
2016-08-15 17:13:14 -0400

    Made the comparisons more effective by precomputing covariances

--- comp.py
+++ comp.py
@@ -2,6 +2,36 @@ import numpy as np
 from scipy.linalg.blas import sgemm
 import time
 
+IN = np.array([])
+OUT = np.array([])
+
+def computeReprs(sn, all_imgs):
+    """Given a list of groups where each contains a list of image names,
+    return feature representations and feature covariances of the corresponding
+    images."""
+    groupFeatures, groupCovs = [], []
+    for group in all_imgs:
+        F_all = []
+        cov_all = []
+        for img in group:
+            F = {}
+            cov = {}
+            sn.load_image(img)
+            sn.net.forward()
+            for l in sn.net.blobs.keys():
+                f = sn.getF(l)
+                mean = np.mean(f, axis=1)
+                f_center = f - np.asmatrix(mean).T
+                f_center = np.asarray(f_center)
+                F[l] = f_center
+                n = f.shape[0]
+                cov[l] = sgemm(1.0/n, f_center, f_center.T)
+            F_all.append(F)
+            cov_all.append(cov)
+        groupFeatures.append(F_all)
+        groupCovs.append(cov_all)
+    return groupFeatures, groupCovs
+
 def f(x):
     if np.isfinite(x):
         return x
@@ -26,13 +56,7 @@ def getG(sn, img, l):
     Fnorm = F / np.matrix(norm).T
     return sgemm(sc, Fnorm, Fnorm.T)
 
-def compare(sn, img1, img2, l):
-    G1 = getG(sn, img1, l)
-    G2 = getG(sn, img2, l)
-    n = G1.shape[0]
-    return np.sum(np.square(G1 - G2)) / n**2 
-
-def computeNorm(sn, img1, img2, l):
+def compareNorm(sn, img1, img2, l):
     G1 = np.log(getG(sn, img1, l))
     G2 = np.log(getG(sn, img2, l))
     G1 = G1 / np.sum(G1)
@@ -40,62 +64,47 @@ def computeNorm(sn, img1, img2, l):
     n = G1.shape[0]
     return np.sum(np.abs(G1 - G2)) / n**2
 
-def _computeCorr(F):
-    #compute the correlation matrix of filters given
-    #an n x k feature response matrix.
-    mean = np.mean(F, axis=1)
-    std = np.std(F, axis=1)
+def computeCov(F):
+    #Compute the covariance matrix of filters given
+    #a mean-centered n x k feature response matrix.
     n = F.shape[0]
-    corr = np.zeros((n, n), dtype=np.float32)
-    for i in range(n):
-        for j in range(n):
-            corr[i,j] = np.dot(F[i] - mean[i], F[j] - mean[j]) / (n**2)
-    corr /= n
-    t = time.time()
-    for i in range(n):
-        for j in range(n):
-            if std[i] == 0 or std[j] == 0:
-                corr[i, j] = 0
-            else:
-                corr[i,j] /= (std[i] * std[j])
-    print "normalizing took " + str(time.time() - t)
-    return corr
+    cov = sgemm(1.0/n, F, F.T)
+    return cov
 
 def computeCorr(F):
     #compute the correlation matrix of filters given
     #a mean-centered n x k feature response matrix.
     std = np.std(F, axis=1)
     n = F.shape[0]
-    t = time.time()
     corr = sgemm(1.0/n**3, F, F.T)
-    #print "computing corr took " + str(time.time() - t)
-    t = time.time()
     std = np.matrix(std)
     std = np.dot(std.T, std)
     std = np.asarray(std)
     corr = corr / std
     corr = infToZero(corr)
-    """for i in range(n):
-        for j in range(n):
-            if std[i] == 0 or std[j] == 0:
-                corr[i, j] = 0
-            else:
-                corr[i,j] /= (std[i] * std[j])"""
-    #print "normalizing took " + str(time.time() - t)
     return corr
 
-def compareF(F1, F2):
-    R1 = computeCorr(F1)
-    R2 = computeCorr(F2)
-    return np.mean(np.abs(R1 - R2))
+def compareF(F1, F2, compMethod):
+    cov1 = compMethod(F1)
+    cov2 = compMethod(F2)
+    return compareCov(cov1, cov2)
 
-def compareImages(l, img1, img2):
+def compareCov(cov1, cov2):
+    return np.mean(np.abs(cov1 - cov2))
+
+def compareImagesGivenFeatures(l, img1, img2):
     F1 = img1[l]
     F2 = img2[l]
+    return compareF(F1, F2, computeCov)
 
-    return compareF(F1, F2)
+def compareImagesGivenCovariances(l, img1, img2):
+    cov1 = img1[l]
+    cov2 = img2[l]
+    return compareCov(cov1, cov2)
 
 def compareGroups(l, group1, group2, same):
+    global IN
+    global OUT
     n = len(group1)
     dist = np.zeros((n, n))
     avg = 0.0
@@ -106,48 +115,36 @@ def compareGroups(l, group1, group2, same):
                 continue
             if same and i == j:
                 continue
-            #print "images ", i, j
             t = time.time()
-            d = compareImages(l, group1[i], group2[j])
-            #print "comparison took " + str(time.time() - t)
+            #d = compareImagesGivenFeatures(l, group1[i], group2[j])
+            d = compareImagesGivenCovariances(l, group1[i], group2[j])
             dist[i,j] = d
             dist[j,i] = d
             avg += d
             count += 1
+            if same:
+                IN = np.append(IN, d)
+            else:
+                OUT = np.append(OUT, d)
     avg /= count
     return avg, dist
 
-def compareManyGroups(sn, layer, groupList):
+def compareManyGroups(sn, layer, groupFeatures):
     """
-    @param groupList List of lists, where each element is an image location.
+    @param groupFeatures list of lists where each element is a dictionary of
+    feature representations for each layer for a particular image
     """
-    n = len(groupList)
+    n = len(groupFeatures)
     dist = np.zeros((n, n))
-    groupFeatures = []
-    for group in groupList:
-        F_all = []
-        for img in group:
-            F = {}
-            sn.load_image(img)
-            sn.net.forward()
-            for l in sn.net.blobs.keys():
-                f = sn.getF(l)
-                #mean-center to save computation later
-                mean = np.mean(f, axis=1)
-                f = f - np.asmatrix(mean).T
-                f = np.asarray(f)
-                F[l] = f
-            F_all.append(F)
-        groupFeatures.append(F_all)
     inGroup = 0.0
     outGroup = 0.0
     inGroupCt = 0
     outGroupCt = 0
+    t = time.time()
     for i in range(n):
         for j in range(n):
             if j > i:
                 continue
-            #print "groups ", i, j
             avg, d = compareGroups(layer, groupFeatures[i], groupFeatures[j], i == j)
             dist[i,j] = avg
             dist[j,i] = avg
@@ -163,28 +160,44 @@ def compareManyGroups(sn, layer, groupList):
     np.set_printoptions(precision=4)
     
     print layer
-    print "Ingroup average: " + str(inGroup)
-    print "Outgroup average: " + str(outGroup)
     print "Ratio: " + str(outGroup / inGroup)
+    stdIn = np.std(IN)
+    stdOut = np.std(OUT)
+    distIn = (np.mean(OUT) - np.mean(IN)) / stdIn
+    distOut = (np.mean(OUT) - np.mean(IN)) / stdOut
+    print "In mean:", np.mean(IN)
+    print "Out mean", np.mean(OUT)
+    print "In std: ", stdIn
+    print "Normalized distance in: ", distIn
+
+    T = time.time() - t
+    n = len(IN) + len(OUT)
+    print "Complete comparison took " + str(T)
+    print "Performed " + str(n) + "comparisons."
+    print "Average comparison took " + str(T/n)
+
+    print ""
             
 def compute():
     import os
     os.environ['GLOG_minloglevel'] = '3'
     import caffe
-    caffe.set_device(2)
+    caffe.set_device(3)
     caffe.set_mode_gpu()
     import numpy as np
     from simple_net import SimpleNet
     
     sn = SimpleNet()
     imgs = ["dog", "cat", "hedgehog", "rabbit", "squirell", "ball"]
-    #imgs = ["dog", "cat"]
     all_imgs = [["categories/" + x + str(i) + ".jpg" for i in range(1, 5)] for x in imgs]
 
-    layers = ["data"]
-    #layers = ["conv5_1"]
+    #layers = ["conv4_1", "conv4_2", "conv4_3", "conv4_4" ,"conv5_1", "conv5_2", "conv5_3", "conv5_4"]
+    layers = ["conv5_4"]
+    groupFeatures, groupCovs = computeReprs(sn, all_imgs)
     for l in layers:
-        compareManyGroups(sn, l, all_imgs)
+        IN = np.array([])
+        OUT = np.array([])
+        compareManyGroups(sn, l, groupCovs)
 
 if __name__ == "__main__":
     compute()
